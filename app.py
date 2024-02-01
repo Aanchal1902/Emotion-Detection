fc8d590339db366bdcf45e82ddf0d40ef92c033c
priya <priyalandge13@gmail.com>
2024-02-01 08:06:33 +0530

    modified:   app.py 	new file:   templates/index.html new file:   templates/login.html 	new file:   templates/reg.html

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template, request, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
+app = Flask(__name__)
+ 
+ 
+@app.route('/')
+@app.route('/login.html')
+def login():
+
+    return render_template("login.html")
+ 
+ 
+@app.route('/register.html')
+def register():
+    return render_template('register.html')
+
+@app.route('/index.html')
+def healtify():
+    return render_template('index.html')
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True,port=8000)
