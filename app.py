136c1f6e447d8d0cb9c276883a8d22cd7a9045e4
priya <priyalandge13@gmail.com>
2024-02-09 15:20:37 +0530

    modified:   app.py 	new file:   templates/feedback.html 	modified:   templates/index.html 	modified:   templates/login.html 	modified:   templates/reg.html

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
  
  
 @app.route('/')
+@app.route('/reg.html')
+def register():
+    return render_template('reg.html')
+
+
 @app.route('/login.html')
 def login():
 
-    return render_template("login.html")
+    return render_template("index.html")
  
  
-@app.route('/register.html')
-def register():
-    return render_template('register.html')
+
 
 @app.route('/index.html')
 def index():
